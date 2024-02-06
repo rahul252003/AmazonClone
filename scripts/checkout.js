7501cacb109e7ddd62c999a01ea1d2fff170aed6
Rahul <rahulsharma2rk2329@gmail.com>
2024-02-06 22:11:53 +0530

    Final

---
+++ scripts/checkout.js
@@ -0,0 +1,6 @@
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import { renderOderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+renderCheckoutHeader();
+renderOderSummary();
+renderPaymentSummary();
